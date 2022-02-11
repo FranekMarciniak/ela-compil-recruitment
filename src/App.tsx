6c22648bd0e4105f8d8d807f1b12257689145fbd
franciszek <prostyfranek@protonmail.com>
2022-02-11 14:40:18 +0100

    basic ws and http api setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,55 @@ import ISmartDevice from "./types/SmartDevice.interface";
 import ISmartBulb from "./types/SmartDevicesDetails/SmartBulb.interface";
 import ISmartOutlet from "./types/SmartDevicesDetails/SmartOutlet.interface";
 import ITempSensor from "./types/SmartDevicesDetails/SmartTemperatureSensor.interface";
+import CardModal from "./components/CardModal";
 
-// import Interactive from "./utils/Interactive";
 function App() {
-  const ws = useRef(
-    new WebSocket("ws://192.168.0.23:3080/api/v1/refresh")
-  ).current;
-  console.log(ws);
   const [devices, setDevices] = useState<ISmartDevice[] | []>([]);
+  const [modal, setModal] = useState(false);
+  const ws = useRef<WebSocket | null>(null);
+
+  const fetchDevices = async () => {
+    try {
+      const res = await fetch("http://localhost:3080/api/v1/devices", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setDevices(await res.json());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
-    ws.onopen = () => {
-      console.log("Connected to the server");
-    };
+    ws.current = new WebSocket("ws://192.168.0.23:3080/api/v1/refresh");
+    fetchDevices();
+    if (ws) {
+      ws.current.onopen = () => {
+        console.log("Connected to the server");
+      };
+    }
   }, []);
+
   return (
     <Layout>
-      <div className={styles.items__grid}>
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-      </div>
+      <>
+        <CardModal open={modal} />
+        <div className={styles.items__grid}>
+          {devices.map((device) => {
+            return (
+              <ItemCard
+                key={device.id}
+                connection={device.connectionState}
+                name={device.name}
+                type={device.type}
+                onClick={() => setModal(!modal)}
+              />
+            );
+          })}
+        </div>
+      </>
     </Layout>
   );
 }
